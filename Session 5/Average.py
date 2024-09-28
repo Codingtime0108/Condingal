b374c4517514477abf81209de05823fe02a20138
Codingtime <13tia.vora@dulwich-seoul.kr>
2024-09-28 17:43:09 +0900

    First

---
+++ Session 5/Average.py
@@ -0,0 +1,10 @@
+t1 = 98
+t2 = 94
+t3 = 96
+t4 = 41
+t5 = 11
+
+sum = t1+t2+t3+t4+t5
+print("the sum of all your numbers is", sum)
+average = sum/5
+print("the mean of all your numbers is", average)
