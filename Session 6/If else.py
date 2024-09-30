365e0480e0543a319af62789bbfb29ffbb54a8f2
Codingtime <13tia.vora@dulwich-seoul.kr>
2024-09-30 17:19:02 +0900

    1st

---
+++ Session 6/If else.py
@@ -0,0 +1,12 @@
+ans = input("When did world war 2 end?")
+if ans == "1945":
+    print("That's correct")
+else:
+    print("The correct answer is 1945")
+
+ans = input("Which data type in coding gives an answer of true or false?")
+ans = (ans.upper())
+if ans == "BOOLEAN":
+    print("That's correct")
+else:
+    print("The correct answer is boolean!")
