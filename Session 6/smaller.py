a4ff824ad1dbb4d9f2d4cafe6ab298f5b01919ed
Codingtime <13tia.vora@dulwich-seoul.kr>
2024-09-30 17:34:00 +0900

    15

---
+++ Session 6/smaller.py
@@ -0,0 +1,7 @@
+small = int(input("Please enter any number from 1-200:"))
+if small<15:
+    print("Your number here is less than 15!")
+elif small>15:
+    print("Your number here is more than 15!")
+else:
+    print("Your number is the same as 15")
