20d48b25ad961836c1b272cdac827c299f4fd13e
Codingtime <13tia.vora@dulwich-seoul.kr>
2024-09-30 17:42:10 +0900

    odd or even

---
+++ Session 6/odd or even.py
@@ -0,0 +1,7 @@
+oden = int(input("Enter any number:"))
+if oden==0:
+    print("The number you have entered is 0 and is neither odd or even")
+elif oden%2==0:
+    print("The number you have entered is even")
+else:
+    print("The number you have entered is odd!")
