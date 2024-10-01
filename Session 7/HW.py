7fbdb922a259cf4c4c7afb14b05ec0b11da549ef
Codingtime <13tia.vora@dulwich-seoul.kr>
2024-10-01 21:35:13 +0900

    HW

---
+++ Session 7/HW.py
@@ -0,0 +1,6 @@
+#input form user#alphabet or not#isaplha#other logic is also fine
+alp= input("Enter anything:")
+if alp.isalpha():
+    print(alp,"is an alphabet")
+else:
+    print(alp,"is an not an alphabet")
