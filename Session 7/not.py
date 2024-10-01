428daf37850cb7e3c1bd69380b62de2c1210a222
Codingtime <13tia.vora@dulwich-seoul.kr>
2024-10-01 14:59:24 +0900

    not

---
+++ Session 7/not.py
@@ -0,0 +1,25 @@
+a=100000
+b=11
+c=11
+
+print(a!=b)
+print(b!=c)
+
+a1= "python"
+b1= "codingal"
+if a1 != b1:
+    print(a1,"and", b1,"are different")
+
+a2=6
+b2=9
+
+if (a2==3)!=(b2==7):
+    print("Nice!")
+else:
+    print("woops")
+
+num= int(input("enter any number"))
+if num%2 !=0:
+    print(num,"is an odd number")
+else:
+    print(num,"is an even number")
