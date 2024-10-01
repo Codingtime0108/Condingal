cbeba5e0c065ca8a79304855498be6fcade25e25
Codingtime <13tia.vora@dulwich-seoul.kr>
2024-10-01 14:49:18 +0900

    2

---
+++ Session 7/logic2.py
@@ -0,0 +1,15 @@
+a = 20
+b = -2
+c = 0
+
+if a>0 or b>0:
+    print("Atleast one of the numbers are greater than 0")
+else:
+    print("No number is greater than 0")
+a1 = 20
+b1 = -2
+c1 = 0
+if c1==0 and b1<0:
+   print("There is a negative number here")
+else:
+    print("All of these variables are positive numbers!")
